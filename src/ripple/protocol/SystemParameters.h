4eeca075190d1e5332d0edd9152b4cfea95978d8
Lehuo Community <community@lehuo>
2016-06-02 16:50:53 +0800

    update readme

--- src/ripple/protocol/SystemParameters.h
+++ src/ripple/protocol/SystemParameters.h
@@ -54,7 +54,7 @@ SYSTEM_CURRENCY_START = SYSTEM_CURRENCY_INITIAL * SYSTEM_CURRENCY_PARTS;
 /** Period 1 for inflation. */
 static
 std::uint64_t const
-SYSTEM_INFLATE_PERIOD_1 = 2 * 31536000ull;  // Two years.
+SYSTEM_INFLATE_PERIOD_1 = 31536000ull;  // First year.
 
 static
 std::uint64_t const
@@ -66,12 +66,12 @@ SYSTEM_INFLATE_MONTH_1 = 10;
 
 static
 std::uint64_t const
-SYSTEM_INFLATE_AMOUNT_1 = 20 * 1000 * 1000 * SYSTEM_CURRENCY_PARTS;
+SYSTEM_INFLATE_AMOUNT_1 = 30 * 1000 * 1000 * SYSTEM_CURRENCY_PARTS;
 
 /** Period 2 for inflation. */
 static
 std::uint64_t const
-SYSTEM_INFLATE_PERIOD_2 = 31536000ull; //  One year.
+SYSTEM_INFLATE_PERIOD_2 = 2 * 31536000ull; //  Next two years.
 
 static
 std::uint64_t const
